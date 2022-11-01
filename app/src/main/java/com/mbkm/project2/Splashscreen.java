62d27d204a4ee7d2e974df0217c7bc02365f6cc2
vianandi <vianandi60@gmail.com>
2022-11-01 12:02:58 +0700

    projek 2

---
+++ app/src/main/java/com/mbkm/project2/Splashscreen.java
@@ -0,0 +1,32 @@
+package com.mbkm.project2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+public class Splashscreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+
+        // inisialisasi view
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.          //untuk menampilkan full screen
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        // Hide ActionBar                //untuk menyembunyikan action bar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splashscreen.this, MainActivity.class));
+                finish();
+            }
+        }, 3500);   // timer
+    }
+}
